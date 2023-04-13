c7321ac2395a9caf7904b904e156f5e62f066087
TDpy <toto.dupuy6558@gmail.com>
2023-04-13 17:51:07 +0200

    adding constructor of BankBuilder, and initializing data

--- java/src/main/java/money_problem/domain/BankBuilder.java
+++ java/src/main/java/money_problem/domain/BankBuilder.java
@@ -8,6 +8,12 @@ public class BankBuilder {
     private Map<Currency, Double> exchangeRate;
     private Currency pivotCurrency;
 
+    public BankBuilder(){
+        this.pivotCurrency = Currency.USD;
+        this.exchangeRate = new HashMap<>();
+        this.exchangeRate.put(Currency.USD, 20.0);
+    }
+
     public void setExchangeRate(Map<Currency, Double> exchangeRate){
         this.exchangeRate = exchangeRate;
     }
